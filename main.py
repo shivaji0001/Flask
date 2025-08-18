d9259b3811ca6703fb370aee54fa21bf39888ed8
shivaji0001 <shivaji0001.ss@gmail.com>
2025-08-18 14:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def load_user(user_id):
 
 
 # app.config['SQLALCHEMY_DATABASE_URL']='mysql://username:password@localhost/databas_table_name'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/studentsdbms'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/psitdbms'
 db=SQLAlchemy(app)
 
 # here we will create db models that is tables
